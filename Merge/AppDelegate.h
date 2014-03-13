046cf9a88ee93133debf5a22255aefe57ef130c9
Jason Fieldman <jason@fieldman.org>
2014-03-13 10:03:12 -0700

    Initial project checking

---
+++ Merge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Merge
+//
+//  Created by Jason Fieldman on 3/13/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
