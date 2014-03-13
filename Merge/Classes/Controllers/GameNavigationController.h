6b5fa5cf3a4bffc175d4ff3b4ba2993e609cc601
Jason Fieldman <jason@fieldman.org>
2014-03-13 13:13:06 -0700

    added standard includes

---
+++ Merge/Classes/Controllers/GameNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  GameNavigationController.h
+//  Merge
+//
+//  Created by Jason Fieldman on 3/13/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameNavigationController : UIViewController
+
+@end
---
+++ Merge/Classes/Controllers/GameNavigationController.m
@@ -0,0 +1,25 @@
+//
+//  GameNavigationController.m
+//  Merge
+//
+//  Created by Jason Fieldman on 3/13/14.
+//  Copyright (c) 2014 Jason Fieldman. All rights reserved.
+//
+
+#import "GameNavigationController.h"
+
+@interface GameNavigationController ()
+
+@end
+
+@implementation GameNavigationController
+
+- (id)init {
+	if ((self = [super init])) {
+
+	}
+	return self;
+}
+
+
+@end
